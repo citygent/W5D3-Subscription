f03184a6c9b42b299671675a219ab9470b9c1966
James White <cyberyt@gmail.com>
2015-09-09 20:46:54 +0100

    can make entries and show page

--- app.rb
+++ app.rb
@@ -1,60 +1,64 @@
-# Dir[File.join(File.dirname(__FILE__), '..',  'models', '**/*.rb')].sort.each do |file|
-#   require file
-# end
-require 'sinatra'
+Dir[File.join(File.dirname(__FILE__), './',  'models', '**/*.rb')].sort.each do |file|
+  require file
+end
 require 'pry'
+# require './models/member'  # see above
+
 
 class Membership < Sinatra::Base
 
   #Lander
   get '/' do
-    redirect to '/player'
+    redirect to '/members'
   end
 
   #INDEX
-  get '/players' do
-    @players = Player.all
+  get '/members' do
+    @members = Member.all
+    erb :home
   end
 
   #NEW
-  # get '/players/new' do
-  #  Not needed we don't need a page for this exercise.
-  # end
+  get '/members/new' do
+    erb :new
+  end
 
   #CREATE
-  post '/players' do
-    hash = {name: "Margherita", sauce: "Tomato", cheese: true, mushrooms: false, extra_toppings: false}
-    @player = Player.new(hash) #Player.new hash
-    if @player.save
-      # redirect to show
+  post '/members' do
+    # binding.pry
+    @member = Member.new params
+    if @member.save
+      redirect to "/members/#{@member.id}"
+    else
+      erb :new
     end
   end
 
   #SHOW
-  get '/players/:id' do
-    @player = Player.find(params[:id]) # or maybe @player = Player.find(:id)
-    # render :show
+  get '/members/:id' do
+    @member = Member.find(params[:id]) # or maybe @member = Member.find(:id)
+    erb :show
   end
 
   #EDIT
-  get '/players/:id/edit' do
-    # @player = Player.find(params[:id])
+  get '/members/:id/edit' do
+    # @member = Member.find(params[:id])
     # Not needed we don't need a page for this exercise.
   end
 
   #UPDATE
-  put '/players/:id' do
+  put '/members/:id' do
     #IRL: changes = {sauce: params[:sauce]}
     changes = {sauce: "Pomodoro"}
-    @player = Player.find(params[:id]) 
-    @player = @player.update(changes)
-    # @player.save < not needed with update.
+    @member = Member.find(params[:id]) 
+    @member = @member.update(changes)
+    # @member.save < not needed with update.
   end
 
   #DELETE
-  delete '/players/:id/delete' do
-    @player = Player.find(params[:id])
-    @player.destroy # :'(
+  delete '/members/:id/delete' do
+    @member = Member.find(params[:id])
+    @member.destroy # :'(
   end
 
 end
